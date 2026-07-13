c7fdd4725534b7472fed76ceab82be85eb873bae
WIZ.Vedanttt ;) <mandevedant11@gmail.com>
2026-07-13 22:42:50 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (2.88%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if len(strs)==0:
+            return ""
+
+        base = strs[0]
+        for i in range(len(base)):
+            for words in strs[1:]:
+                if (i==len(words) or base[i]!=words[i]):
+                    return base[0:i]
+
+        return base
+        
