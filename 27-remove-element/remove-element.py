a02b9682c9c5cd2040403138f65296ef518c693f
WIZ.Vedanttt ;) <mandevedant11@gmail.com>
2026-07-14 21:09:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (89.15%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,12 +1,11 @@
 class Solution(object):
     def removeElement(self, nums, val):
         i = 0
-        k=0
 
         while i < len(nums):
             if(nums[i]==val):
                 nums.pop(i)
-                k+=1
+                
             else:
                 i+=1
 
