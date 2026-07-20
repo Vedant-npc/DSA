b8fc58814433b73fb35df487bb95f8c1404b8126
WIZ.Vedanttt ;) <mandevedant11@gmail.com>
2026-07-20 19:41:23 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (88.78%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def plusOne(self, digits):
+        i = len(digits) - 1
+        while i>=0:
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+
+            digits[i] = 0
+            i-=1
+
+        digits.insert(0,1)
+        return digits
+
+            
+                
+         
