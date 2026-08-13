e7b116807e8dba44807dcb235b07a2f7aa6b3ba3
WIZ.Vedanttt ;) <mandevedant11@gmail.com>
2026-08-13 21:22:34 +0530

    Time: 2 ms (97.32%) | Memory: 12.6 MB (35.38%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def removeOuterParentheses(self, s):
+        stack = []
+        count = 0
+
+        for ch in s:
+            if ch == "(":
+                if count > 0:
+                    stack.append(ch)
+                count +=1
+
+            else:
+                count-=1
+                if count > 0:
+                    stack.append(ch)
+
+        return "".join(stack)
+
+        
